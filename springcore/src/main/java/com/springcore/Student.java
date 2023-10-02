3141298c600564b107d06af683d85ce34d38c045
aniket jain <aniketkjain1@gmail.com>
2023-10-02 20:51:43 +0530

    New Maven Project | Adding Spring Dependencies | Create Config File | Setter Injection

---
+++ springcore/src/main/java/com/springcore/Student.java
@@ -0,0 +1,46 @@
+package com.springcore;
+
+public class Student {
+
+	private int StudentId;
+	private String studentName;
+	private String studentAddress;
+	public int getStudentId() {
+		return StudentId;
+	}
+	public void setStudentId(int studentId) {
+		System.out.println("setting student id");
+		StudentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		System.out.println("setting student name");
+		this.studentName = studentName;
+	}
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+	public void setStudentAddress(String studentAddress) {
+		System.out.println("setting student Address");
+		this.studentAddress = studentAddress;
+	}
+	public Student(int studentId, String studentName, String studentAddress) {
+		super();
+		StudentId = studentId;
+		this.studentName = studentName;
+		this.studentAddress = studentAddress;
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Student [StudentId=" + StudentId + ", studentName=" + studentName + ", studentAddress=" + studentAddress
+				+ "]";
+	}
+	
+	
+}
