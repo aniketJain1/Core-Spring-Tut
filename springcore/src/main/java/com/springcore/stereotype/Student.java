1049fc2430a9bd19ff02e0b03a11ff6b3f19f8f3
aniket jain <aniketkjain1@gmail.com>
2023-10-05 19:53:38 +0530

    Stereotype Annotations | @Component Annotation | @Value Annotation

---
+++ springcore/src/main/java/com/springcore/stereotype/Student.java
@@ -0,0 +1,32 @@
+package com.springcore.stereotype;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Component("obj")
+public class Student {
+
+	@Value("Aniket")
+	private String name;
+	@Value("delhi")
+	private String city;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", city=" + city + "]";
+	}
+	
+	
+}
