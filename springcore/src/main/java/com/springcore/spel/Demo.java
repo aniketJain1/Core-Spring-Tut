6cc76bd57a01e997d11e65e932074002dcc7ff20
aniket jain <aniketkjain1@gmail.com>
2023-10-06 12:18:47 +0530

    Spring Expression Language | SpEL | what is SpEL | How to use Expression

---
+++ springcore/src/main/java/com/springcore/spel/Demo.java
@@ -0,0 +1,37 @@
+package com.springcore.spel;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Demo {
+	
+	@Value("#{5+10}")
+	private int x;
+	
+	@Value("#{5+9+4+22}")
+	private int y;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		return "Demo [x=" + x + ", y=" + y + "]";
+	}
+
+
+}
